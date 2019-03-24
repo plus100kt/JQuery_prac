4b007a96ca44db20b7ab85eab3d1175ef7eb02e2
plus100kt <plus100kt@gmail.com>
2019-03-24 16:37:34 +0900

    day2 updated

--- day1/master.js
+++ day1/master.js
@@ -1,3 +1,4 @@
+// body의 class를 dark로 지정해준다
 function func() {
   document.body.className = "dark";
 }
--- day2/master.js
+++ day2/master.js
@@ -1,3 +1,13 @@
-function func() {
-  document.body.className = "dark";
-}
+// onclick 과의 차이
+// onclick은 하나의 콜백만 지정가능
+// addEventListener는 여러개의 이벤트 리스너 추가 가능, 분리의 측면에서도 유리
+
+window.onload = function() {
+  var hw = document.getElementById('hw');
+  hw.addEventListener('click', function() {
+    alert('Hello world');
+  });
+};
+
+// window.onload는 html코드를 전부 읽어들인 다음 실행
+// 서순을 지키기위해 window.onload실행
